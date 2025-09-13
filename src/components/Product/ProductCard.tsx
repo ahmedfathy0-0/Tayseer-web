69e4472fe8871c04993a1b57ee4dd86cee1448d9
ahmedfathy0-0 <ahmedfathi20044002@gmail.com>
2025-09-13 05:28:14 +0300

    feat: Update SVG path in ProductCard for improved icon representation

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -173,10 +173,10 @@ const ProductCard = ({
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M16 11V7a4 4 0 00-8 0v4M8 11v6a2 2 0 002 2h4a2 2 0 002-2v-6M8 11h8"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-2.293 2.293c-.63.63-.184 1.707.707 1.707H19M7 13v4a2 2 0 002 2h4a2 2 0 002-2v-4m-6 4h.01M15 17h.01"
               />
             </svg>
             Order Now
